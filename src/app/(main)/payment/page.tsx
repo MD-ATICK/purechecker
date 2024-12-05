afd557ddccd9b1b504346bf5a63cbc222a1b3f10
MD Atick <atick.business.info@gmail.com>
2024-12-05 23:54:51 +0600

    payment 3

--- src/app/(main)/payment/page.tsx
+++ src/app/(main)/payment/page.tsx
@@ -1,39 +1,59 @@
 "use client"
 
 import { Button } from "@/components/ui/button";
-import { useEffect } from "react";
+import { initializePaddle, Paddle } from '@paddle/paddle-js';
+import { useEffect, useState } from "react";
 
 export default function Test() {
 
-  const handlePayment = async () => {
-    if (window.Paddle) {
-      await window.Paddle.Checkout.open({
-        items: [
-          {
-            priceId: 'pri_01je3x5306ed7aqpfmy39awjdp',
-            quantity: 1
-          }
-        ],
-        customer: {
-          email: 'customer@gmail.com'
-        }
-      })
-    }
-  }
+  const [paddle, setPaddle] = useState<Paddle>();
+  
+  // const handlePayment = async () => {
+  //   if (window.Paddle) {
+  //     await window.Paddle.Checkout.open({
+  //       items: [
+  //         {
+  //           priceId: 'pri_01je3x5306ed7aqpfmy39awjdp',
+  //           quantity: 1
+  //         }
+  //       ],
+  //       customer: {
+  //         email: 'customer@gmail.com'
+  //       }
+  //     })
+  //   }
+  // }
+
+  const handlePayment = () => {
+    paddle?.Checkout.open({
+      items: [{ priceId: 'pri_01je3x5306ed7aqpfmy39awjdp', quantity: 1 }],
+    });
+  };
+
+  // useEffect(() => {
+  //   if (window.Paddle) {
+  //     window.Paddle.Initialize({
+  //       token: 'live_f34a0edff531556f462721cf531',
+  //     })
+  //     window.Paddle.Environment.set('sandbox');
+  //   }
+    
+  // }, []);
 
   useEffect(() => {
-    if (window.Paddle) {
-      window.Paddle.Environment.set('sandbox');
-      window.Paddle.Initialize({
-        token: 'live_f4dc96c4f979c2462dc8d759c41'
-      })
-    }
+    initializePaddle({ environment: 'sandbox', token: 'live_f34a0edff531556f462721cf531' }).then(
+      (paddleInstance: Paddle | undefined) => {
+        if (paddleInstance) {
+          setPaddle(paddleInstance);
+        }
+      },
+    );
   }, []);
 
   return (
     <div className=" container mx-auto p-10">
       <Button onClick={handlePayment}>
-        Buy Plan
+        Buy Plan 1
       </Button>
     </div>
   )
