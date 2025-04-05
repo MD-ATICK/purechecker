af53c18b656ccae42c920d35e681e9cfa30ef6bc
MD Atick <115481550+MD-ATICK@users.noreply.github.com>
2025-04-05 20:55:45 +0600

    error slove

--- app/(main)/(user)/user/verify-emails/test-upload-file/complete-file-to-download.tsx
+++ app/(main)/(user)/user/verify-emails/test-upload-file/complete-file-to-download.tsx
@@ -1,26 +1,8 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { generateXLSX } from "@/utils/BulkConvertFile";
 import { CheckCheck } from "lucide-react";
-import React, { useTransition } from "react";
-import { getVerifyEmailsByFileId } from "./actions";
 import DownloadFileDrawer from "./components/download-file-drawer";
 
 export default function CompleteFileToDownload({ fileId }: { fileId: string }) {
-  const randomTenDigit = () => {
-    return Math.floor(1000000000 + Math.random() * 9000000000).toString();
-  };
-
-  const [isPending, startTransaction] = useTransition();
-
-  const handleDownload = async () => {
-    startTransaction(async () => {
-      const { data } = await getVerifyEmailsByFileId(fileId);
-      if (data) {
-        generateXLSX(randomTenDigit(), data);
-      }
-    });
-  };
 
   return (
     <div>
@@ -28,10 +10,7 @@ export default function CompleteFileToDownload({ fileId }: { fileId: string }) {
         <CheckCheck />
         Completed
       </div>
-
-      <Button onClick={handleDownload} disabled={isPending}>
-        {isPending ? "Downloading..." : "skip"}
-      </Button>
+      <br />
       <DownloadFileDrawer fileId={fileId} />
     </div>
   );
