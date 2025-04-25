a57633b6b5e2245059e9ad0f12027e3308613b50
MD-ATICK <atick.business.info@gmail.com>
2025-04-26 02:04:32 +0600

    update perfom list

--- app/api/zapier/perform-list/route.ts
+++ app/api/zapier/perform-list/route.ts
@@ -2,7 +2,22 @@ import { db } from "@/lib/prisma";
 
 export async function GET() {
 	try {
-		const data = await db.verifyEmail.findMany({ take: 2 });
+		const data = await db.verifyEmail.findMany({
+			take: 2,
+			select: {
+				email: true,
+				domain: true,
+				reason: true,
+				isExist: true,
+				isDisposable: true,
+				isValidSyntax: true,
+				isValidDomain: true,
+				riskLevel: true,
+				free: true,
+				role: true,
+				mxRecords: true,
+			},
+		});
 		return new Response(JSON.stringify(data), {
 			status: 200,
 		});
